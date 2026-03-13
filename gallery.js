8a16b4d14bcaa0cda017360d998114b1c4a2945a
FtRookie <236965164+FtRookie@users.noreply.github.com>
2026-03-12 18:51:01 -0700

    yay im dependent on somebody now

---
+++ gallery.js
@@ -0,0 +1,13 @@
+const lightbox = GLightbox({
+    selector: '.glightbox',
+    touchNavigation: true,
+    loop: true,
+    zoomable: true,
+    draggable: true,
+    closeButton: true,
+    closeOnOutsideClick: true,
+    openEffect: "none",
+    closeEffect: "none",
+    slideEffect: "none",
+    preload: true,
+});
