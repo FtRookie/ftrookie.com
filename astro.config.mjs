61d2565c93cae58c3ae83cff336c42607d25ed6d
FtRookie <236965164+FtRookie@users.noreply.github.com>
2026-03-14 11:34:13 -0700

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import node from '@astrojs/node';
 
 export default defineConfig({
   site: "https://www.ftrookie.com",
+  base: 'home',
   trailingSlash: "never",
+  output: 'static',
   adapter: node({
     mode: 'standalone',
   }),
