596289573c9c2b24016ec609069dbfa4de3d22f2
FtRookie <236965164+FtRookie@users.noreply.github.com>
2026-03-14 11:29:41 -0700

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.ftrookie.com",
   trailingSlash: "never",
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
