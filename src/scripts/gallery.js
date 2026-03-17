7dd0cb99cee1c5d031c0ce48d57d6de0432a1a17
FtRookie <236965164+FtRookie@users.noreply.github.com>
2026-03-17 03:58:01 -0700

    fix gallery

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,14 +1,16 @@
-const lightbox = GLightbox({
-    selector: '.glightbox',
-    touchNavigation: true,
-    loop: true,
-    zoomable: true,
-    draggable: true,
-    closeButton: true,
-    closeOnOutsideClick: true,
-    openEffect: "none",
-    closeEffect: "none",
-    slideEffect: "none",
-    preload: true,
-    keyboardNavigation: true,
+document.addEventListener("astro:page-load", () => {
+    const lightbox = GLightbox({
+        selector: '.glightbox',
+        touchNavigation: true,
+        loop: true,
+        zoomable: true,
+        draggable: true,
+        closeButton: true,
+        closeOnOutsideClick: true,
+        openEffect: "none",
+        closeEffect: "none",
+        slideEffect: "none",
+        preload: true,
+        keyboardNavigation: true,
+    });
 });
